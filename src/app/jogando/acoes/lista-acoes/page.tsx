58f41df1031c09ec7232c95de68c22958c5aa210
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2024-07-15 11:27:09 -0300

    change: Erro de digitação

--- src/app/jogando/acoes/lista-acoes/page.tsx
+++ src/app/jogando/acoes/lista-acoes/page.tsx
@@ -7,7 +7,7 @@ export default function Page_ListarAcoes() {
 	return <main>
 		<h2>Ações em Combate</h2>
 		<p>
-			As Ações descritas nessa sessão podem ser usadas durante um combate por
+			As Ações descritas nessa seção podem ser usadas durante um combate por
 			qualquer personagem. Outras Ações só se tornam disponíveis através de
 			Habilidades. Com a permissão do Mestre, você é livre para usar Ações
 			diferentes das descritas aqui, contanto que sejam possíveis para o seu
@@ -17,7 +17,7 @@ export default function Page_ListarAcoes() {
 
 		<h2>Reações em Combate</h2>
 		<p>
-			As Reações descritas nessa sessão podem ser usadas durante um combate
+			As Reações descritas nessa seção podem ser usadas durante um combate
 			por qualquer personagem. Outras Reações só se tornam disponíveis
 			através de Habilidades.
 		</p>
