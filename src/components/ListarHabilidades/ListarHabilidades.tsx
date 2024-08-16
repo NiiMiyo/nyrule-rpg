73f9a7d916bbc071d683cb20f4022444e9153f8b
Douglas Sebastian <douglas.sebastian.mme@gmail.com>
2024-08-16 15:01:26 -0300

    new: filtro em ListarHabilidades

--- src/components/ListarHabilidades/ListarHabilidades.tsx
+++ src/components/ListarHabilidades/ListarHabilidades.tsx
@@ -1,14 +1,24 @@
 import { HTMLAttributes } from "react";
 import { HABILIDADES_DADOS } from "@/data/habilidades";
+import { Habilidade } from "@/typing";
 import { RenderHabilidade } from "./RenderHabilidade";
 
 
 export function ListarHabilidades( props : Readonly<Props_ListarHabilidades> ) {
-	return <section { ...props.attrs }>
-		{ HABILIDADES_DADOS.map(h => <RenderHabilidade habilidade={h} key={h.nome} />) }
+	const { filtro, ...attrs } = props;
+
+	const habilidades_filtradas = filtro !== undefined
+		? HABILIDADES_DADOS.filter(filtro)
+		: HABILIDADES_DADOS;
+
+	return <section { ...attrs }>
+		{ habilidades_filtradas
+			.map(h => <RenderHabilidade habilidade={h} key={h.nome} />)
+		}
 	</section>
 };
 
 export type Props_ListarHabilidades = {
-	attrs?: HTMLAttributes<HTMLTableSectionElement>
+	attrs?: HTMLAttributes<HTMLTableSectionElement>;
+	filtro?(h: Habilidade): boolean;
 }
